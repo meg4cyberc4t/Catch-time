771d371d3b48030f051c0bd30288cf887be81d10
meg4cyberc4t <66907532+meg4cyberc4t@users.noreply.github.com>
2022-04-03 12:25:40 +0300

    Механика подбора вещей

--- Assets/Scripts/CircleScript.cs
+++ Assets/Scripts/CircleScript.cs
@@ -6,21 +6,29 @@ namespace Assets.Scripts
 {
     public class CircleScript : MonoBehaviour
     {
+        private bool _characterNearby = false;
+        private void FixedUpdate()
+        {
+            if (!_characterNearby) return;
+            if (!Input.GetKeyDown(KeyCode.E)) return;
+            GetComponent<AudioSource>().Play();
+            Destroy(gameObject);
+            Debug.Log("Убрали чела");
+        }
+        
         private void OnTriggerEnter2D([NotNull] Collider2D other)
         {
             if (other == null) throw new ArgumentNullException(nameof(other));
             if (other.tag != "Player") return;
-            GetComponent<AudioSource>().Play();
-            Debug.Log("Чел в зоне достигаемости " + other.name);
+            _characterNearby = true;
+            
         }
         
         private void OnTriggerExit2D([NotNull] Collider2D other)
         {
             if (other == null) throw new ArgumentNullException(nameof(other));
-            if (other.tag == "Player")
-            {
-                Debug.Log("Чел вне зоны достигаемости " + other.name);
-            }
+            if (other.tag != "Player") return;
+            _characterNearby = true;
         }
     }
 }
