bdfd44eebedba8fdf286d77ada1d4553e3eda6d9
meg4cyberc4t <66907532+meg4cyberc4t@users.noreply.github.com>
2022-04-03 13:11:18 +0300

    Незаконченная ходьба кошкодевочки

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -4,33 +4,40 @@ namespace Assets.Scripts
 {
     public class CharacterController : MonoBehaviour {
         public float MaxSpeed = 6f;
-        
+
+        private SpriteRenderer _spriteRenderer;
         private float _move;
-        private bool _facingRight = true;
 
+        private Animator _anim;
 
+        private void Start()
+        {
+            _anim = GetComponent<Animator>();
+            _spriteRenderer = GetComponent<SpriteRenderer>();
+        }
+        
         private void FixedUpdate () {
             _move = Input.GetAxis("Horizontal");
         }
 
         private void Update(){
             GetComponent<Rigidbody2D>().velocity = new Vector2 (_move * MaxSpeed, GetComponent<Rigidbody2D>().velocity.y);
-            if (Input.GetKey(KeyCode.LeftShift)) return;
+            
             switch (_move)
             {
-                case > 0 when !_facingRight:
-                case < 0 when _facingRight:
-                    Flip ();
+                case > 0:
+                    _spriteRenderer.flipX = false;
+                    _anim.SetBool("IsMove", true);
+                    break;
+                case < 0:
+                    _spriteRenderer.flipX = true;
+                    _anim.SetBool("IsMove", true);
+                    break;
+                default:
+                    _anim.SetBool("IsMove", false);
                     break;
             }
 
-        }
-
-        private void Flip(){
-            _facingRight = !_facingRight;
-            var theScale = transform.localScale;
-            theScale.x *= -1;
-            transform.localScale = theScale;
         }
     }
 }
