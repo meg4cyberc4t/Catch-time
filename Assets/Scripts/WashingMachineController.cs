438cc6aee6e42e747f20257602d8575c11901132
LastFlow <somemoredevac@gmail.com>
2022-04-05 02:13:36 +0300

    washing machine

--- Assets/Scripts/WashingMachineController.cs
+++ Assets/Scripts/WashingMachineController.cs
@@ -8,6 +8,9 @@ namespace Assets.Scripts
     public class WashingMachineController : MonoBehaviour
     {
         private GameObject _ebutton;
+        private GameObject norm;
+        private GameObject good;
+        private GameObject bad;
 
         public int AllClothCounter;
         
@@ -17,6 +20,9 @@ namespace Assets.Scripts
         private void Start()
         {
             _ebutton = gameObject.transform.GetChild(0).gameObject;
+            norm = gameObject.transform.GetChild(1).gameObject;
+            good = gameObject.transform.GetChild(2).gameObject;
+            bad = gameObject.transform.GetChild(3).gameObject;
         }
         
         private void OnTriggerStay2D([NotNull] Collider2D other)
@@ -31,6 +37,26 @@ namespace Assets.Scripts
                 AllClothCounter += characterController.GetCapacity();
                 characterController.SetCapacity(0);
                 Debug.Log("Добрая мама");
+
+                if(AllClothCounter < 5)
+                {
+
+                    bad.GetComponent<SpriteRenderer>().color = new Color(255,255,255,1);
+                }
+                else if(AllClothCounter < 10)
+                {
+
+                    norm.GetComponent<SpriteRenderer>().color = new Color(255,255,255,1);
+                    good.GetComponent<SpriteRenderer>().color = new Color(255,255,255,0);
+                    bad.GetComponent<SpriteRenderer>().color = new Color(255,255,255,0);
+                }
+                else
+                {
+                    norm.GetComponent<SpriteRenderer>().color = new Color(255,255,255,0);
+                    good.GetComponent<SpriteRenderer>().color = new Color(255,255,255,1);
+                    bad.GetComponent<SpriteRenderer>().color = new Color(255,255,255,0);
+                }
+
             }
         }
         
