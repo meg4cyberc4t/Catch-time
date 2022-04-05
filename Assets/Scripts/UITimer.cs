6e159844b6cb6a0e45dae865141574b6f1d960e3
LastFlow <somemoredevac@gmail.com>
2022-04-05 03:10:18 +0300

    ffff

--- Assets/Scripts/UITimer.cs
+++ Assets/Scripts/UITimer.cs
@@ -3,17 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
+using Assets.Scripts;
+
 
 public class UITimer : MonoBehaviour
 {
     [SerializeField] private float time;
     [SerializeField] private TextMeshProUGUI timerText;
 
+
     private float _timeLeft = 120f;
     private bool _timerOn = false;
+    
+
+
+    //GameObject.Find("InGameUI/counter/fd").GetComponent<TextMeshProUGUI>();
+
+    WashingMachineController controller; 
  
     private void Start()
     {
+        controller = GameObject.Find("WashingMachine").gameObject.GetComponent<WashingMachineController>();
+
+        GameObject.Find("cutscene_about/counter/").GetComponent<>();
+      
+
         timerText = gameObject.GetComponent<TextMeshProUGUI>();
         _timeLeft = time;
         _timerOn = true;
@@ -21,16 +35,26 @@ public class UITimer : MonoBehaviour
  
     private void Update()
     {
+
+
         if (_timerOn)
         {
             if (_timeLeft > 0)
             {
                 _timeLeft -= Time.deltaTime;
                 UpdateTimeText();
-            } else
+            } 
+            else
             {
                 _timeLeft = time;
                 _timerOn = false;
+            }
+        }
+        else
+        {
+            if(controller.AllClothCounter < 5)
+            {
+
             }
         }
     }
@@ -44,4 +68,7 @@ public class UITimer : MonoBehaviour
         float seconds = Mathf.FloorToInt(_timeLeft % 60);
         timerText.SetText(string.Format("{0:00} : {1:00}", minutes, seconds));
     }
+
+
+
 }
