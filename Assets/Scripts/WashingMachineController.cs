6fd139a342f4be2dfa345687389115dd5d9ef37b
meg4cyberc4t <66907532+meg4cyberc4t@users.noreply.github.com>
2022-04-05 23:55:45 +0300

    Fix WashingMachine

--- Assets/Scripts/WashingMachineController.cs
+++ Assets/Scripts/WashingMachineController.cs
@@ -14,10 +14,12 @@ namespace Assets.Scripts
 
         public Transform camera;
         
-
+        
         public int AllClothCounter;
         
         private Animator _playerAnimator;
+
+        [CanBeNull] private GameObject user;
 
   
         private void Start()
@@ -28,58 +30,53 @@ namespace Assets.Scripts
             bad = gameObject.transform.GetChild(3).gameObject;
         }
 
-        private void OnTriggerStart2D(Collider2D other)
+        private void OnTriggerEnter2D(Collider2D other)
         {
-            OnTriggerStay2D(other);
-        }
-        
-        private void OnTriggerStay2D([NotNull] Collider2D other)
-        {
+            user = other.gameObject;
             _ebutton.GetComponent<SpriteRenderer>().color = new Color(255,255,255,1);
-            if (other.tag == "Player" && Input.GetKeyDown(KeyCode.E))
+        }
+
+        private void OnTriggerExit2D(Collider2D other)
+        {
+            user = null;
+            _ebutton.GetComponent<SpriteRenderer>().color = new Color(255,255,255,0);
+        }
+
+        private void Update()
+        {
+            if (user == null) return;
+            _ebutton.GetComponent<SpriteRenderer>().color = new Color(255,255,255,1);
+            if (!Input.GetKeyDown(KeyCode.E)) return;
+            CharacterController characterController =
+                user.GetComponent<CharacterController>();
+            characterController.Inventory = new List<GameObject>();
+            AllClothCounter += characterController.GetCapacity();
+            characterController.SetCapacity(0);
+
+            if(AllClothCounter < 4)
             {
+                bad.GetComponent<SpriteRenderer>().color = new Color(255,255,255,1);
+            }
+            else if(AllClothCounter < 8)
+            {
+
+                norm.GetComponent<SpriteRenderer>().color = new Color(255,255,255,1);
+                good.GetComponent<SpriteRenderer>().color = new Color(255,255,255,0);
+                bad.GetComponent<SpriteRenderer>().color = new Color(255,255,255,0);
+            }
+            else
+            {
+                norm.GetComponent<SpriteRenderer>().color = new Color(255,255,255,0);
+                good.GetComponent<SpriteRenderer>().color = new Color(255,255,255,1);
+                bad.GetComponent<SpriteRenderer>().color = new Color(255,255,255,0);
+            }
                 
-                CharacterController characterController =
-                    other.gameObject.GetComponent<CharacterController>();
-                characterController.Inventory = new List<GameObject>();
-                AllClothCounter += characterController.GetCapacity();
-                characterController.SetCapacity(0);
-
-                if(AllClothCounter < 4)
-                {
-                    bad.GetComponent<SpriteRenderer>().color = new Color(255,255,255,1);
-                }
-                else if(AllClothCounter < 8)
-                {
-
-                    norm.GetComponent<SpriteRenderer>().color = new Color(255,255,255,1);
-                    good.GetComponent<SpriteRenderer>().color = new Color(255,255,255,0);
-                    bad.GetComponent<SpriteRenderer>().color = new Color(255,255,255,0);
-                }
-                else
-                {
-                    norm.GetComponent<SpriteRenderer>().color = new Color(255,255,255,0);
-                    good.GetComponent<SpriteRenderer>().color = new Color(255,255,255,1);
-                    bad.GetComponent<SpriteRenderer>().color = new Color(255,255,255,0);
-                }
-                
-                if (AllClothCounter == 12)
-                {
-                    camera.position = new Vector3(-77, -13, -20);
-                    Time.timeScale = 0;
-                    Ending.GetEnding(AllClothCounter, StartCoroutine);
-                }
-
+            if (AllClothCounter == 12)
+            {
+                camera.position = new Vector3(-77, -13, -20);
+                Time.timeScale = 0;
+                Ending.GetEnding(AllClothCounter, StartCoroutine);
             }
         }
-        
-        private void OnTriggerExit2D([NotNull] Collider2D other)
-        {
-            if (other.tag == "Player")
-            {
-                _ebutton.GetComponent<SpriteRenderer>().color = new Color(255,255,255,0);
-            } 
-        }
     }
-    
 }
